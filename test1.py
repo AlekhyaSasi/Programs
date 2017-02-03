010c4d443442ddedd50b21ee83175bd22a6b2fb3
Alekhya Sai <alekhyasaip@gmail.com>
2017-02-02 17:37:35 -0800

    basics
    Includes functions ,variables, datatypes

---
+++ test1.py
@@ -0,0 +1,36 @@
+print("first program")
+print ('Hey there')
+print('Alekhya'.replace('A', 'S'))
+replaced='Alekhya'.replace('A', 'S')
+print(replaced)
+currency=145
+currency_today=100.5
+currency1='nothing'
+print(currency,currency_today, currency1)
+
+def currrency_converter(rate, euros):
+    dollars=euros*rate
+    return dollars
+
+print(currrency_converter(100, 1000))
+
+print(1)
+a=2
+b=1
+a=1
+print(b)
+print(a)
+
+def triangle_area(k,h):
+    ar=((k*h)/2)
+    return ar
+
+print(triangle_area(3,4))
+print(type(a))
+d=23.4567
+print(type(d))
+e=57j
+print(type(e))
+print(float(a))
+print(int(d))
+print(round(d))
