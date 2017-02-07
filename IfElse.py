9fb352cd5ca3c38318bf1a98069ea5ff0d66794e
Alekhya Sai <alekhyasaip@gmail.com>
2017-02-06 17:41:01 -0800

    If Else Conditional Statements

---
+++ IfElse.py
@@ -0,0 +1,10 @@
+#Conditional statements
+#If block
+v = int(input("Enter a value:"))
+if v<3:
+     #indentation is important. usually itt's 4 blank spaces
+     print("The value is less than 3")
+elif v==3:
+    print("the value is equal to 3")
+else:
+    print("the value is greater")
