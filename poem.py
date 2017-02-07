80ba325d0aa7b75b49bf3cf7cc958613272e7a67
Alekhya Sai <alekhyasaip@gmail.com>
2017-02-06 17:19:40 -0800

    Exercise on Functions

---
+++ poem.py
@@ -0,0 +1,11 @@
+#Write a program, poem.py, that defines a function that prints a short poem or song verse. Give a meaningful name to the function. Have the program end by calling the function three times, so the poem or verse is repeated three times.
+def poem():
+    print("There is a place where the sidewalk ends")
+    print("And before the street begins,")
+    print("And there the grass grows soft and white,")
+    print("And there the sun burns crimson bright,")
+    print("And there the moon-bird rests from his flight")
+    print("To cool in the peppermint wind.")
+
+for count in [1,2,3]:
+    poem()
