e70e61e1eaf8e07da5704484f058e8e2c6f97332
Alekhya Sai <alekhyasaip@gmail.com>
2019-11-16 19:31:58 -0800

    Create search_in_rotated_array.py
    New prob

---
+++ Python/search_in_rotated_array.py
@@ -0,0 +1,2 @@
+#Given a sorted and rotated array A of N distinct elements which is rotated at some point, and given an element K. The task is to find the index of the given element K in the array A.
+#https://algorithmsandme.com/find-element-in-sorted-rotated-array/
